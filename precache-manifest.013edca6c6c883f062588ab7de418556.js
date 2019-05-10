947d002b666df23ab3e7363b3955c77959c377a2
yushengc <ntuyusheng@gmail.com>
2019-05-10 20:48:25 +0800

    Updates

---
+++ precache-manifest.013edca6c6c883f062588ab7de418556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf18bf62004882e25531a78ad450b399",
+    "url": "/pedroid/testTWMap/index.html"
+  },
+  {
+    "revision": "795de17e564915727464",
+    "url": "/pedroid/testTWMap/static/css/main.c50c7fd5.chunk.css"
+  },
+  {
+    "revision": "4edfa74828c55f7929ca",
+    "url": "/pedroid/testTWMap/static/js/2.57bd5f5a.chunk.js"
+  },
+  {
+    "revision": "631ad55583fb91b89a1c",
+    "url": "/pedroid/testTWMap/static/js/3.1d32a947.chunk.js"
+  },
+  {
+    "revision": "0c18d6cdb02fbaa039f8",
+    "url": "/pedroid/testTWMap/static/js/4.727ea69a.chunk.js"
+  },
+  {
+    "revision": "795de17e564915727464",
+    "url": "/pedroid/testTWMap/static/js/main.09a293f2.chunk.js"
+  },
+  {
+    "revision": "3f6156f0279a527ec4c8",
+    "url": "/pedroid/testTWMap/static/js/runtime~main.8a40514f.js"
+  }
+]);
